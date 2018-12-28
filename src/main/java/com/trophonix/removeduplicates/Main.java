61ac3b21c275b20138fe144c3f558edc4d09d9f7
Lucas Cosolo <502badgamer@gmail.com>
2018-12-27 20:58:14 -0600

    More licensing information

--- src/main/java/com/trophonix/removeduplicates/Main.java
+++ src/main/java/com/trophonix/removeduplicates/Main.java
@@ -5,15 +5,35 @@ import org.apache.commons.io.IOUtils;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
+
+/*
+    RemoveDuplicates is a simple program which removes duplicate files in the working directory.
+    Copyright (C) 2018 Trophonix
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 
 public class Main {
 
   public static void main(String... args) throws IOException {
+    System.out.println(" * - * - * - * \n");
+    System.out.println("    RemoveDuplicates  Copyright (C) 2018  Trophonix\n" +
+            "    This program comes with ABSOLUTELY NO WARRANTY.\n" +
+            "    This is free software, and you are welcome to redistribute it\n");
+    System.out.println(" * - * - * - * \n");
     File root = new File(System.getProperty("user.dir"));
     System.out.println("Scanning [" + root.getName() + "] for duplicates.");
     File[] children = root.listFiles();
